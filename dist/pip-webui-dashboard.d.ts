1656d011cb805d59aae7d774e8473aad1c488ee4
Anastas Fonotov <anastasf@gmail.com>
2017-04-12 22:49:18 +0300

    'rebuild'

--- dist/pip-webui-dashboard.d.ts
+++ dist/pip-webui-dashboard.d.ts
@@ -1,18 +1,5 @@
 declare module pip.dashboard {
 
-export interface IDashboardTile {
-    options: any;
-    color: string;
-    size: Object | string | number;
-}
-export class DashboardTile implements IDashboardTile {
-    options: any;
-    color: string;
-    size: Object | string | number;
-    constructor();
-}
-
-
 export class AddTileDialog {
     title: string;
     icon: string;
@@ -39,6 +26,19 @@ export interface IAddTileDialogprovider {
     configWidgetList(list: [AddTileDialog[]]): void;
 }
 
+
+
+export interface IDashboardTile {
+    options: any;
+    color: string;
+    size: Object | string | number;
+}
+export class DashboardTile implements IDashboardTile {
+    options: any;
+    color: string;
+    size: Object | string | number;
+    constructor();
+}
 
 export class TileConfigDialogController {
     params: any;
@@ -114,13 +114,13 @@ export class DragTileService implements IDragTileService {
 
 
 
-
 export class MenuTileService extends DashboardTile {
     menu: any;
     constructor();
     callAction(actionName: any, params: any, item: any): void;
     changeSize(params: any): void;
 }
+
 
 
 
