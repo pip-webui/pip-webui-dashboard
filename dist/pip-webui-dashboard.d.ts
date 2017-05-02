df518f77391bb31a7b2aa0696d8ebf05f6952414
Anastas Fonotov <anastasf@gmail.com>
2017-05-02 16:20:43 +0300

    'rebuild'

--- dist/pip-webui-dashboard.d.ts
+++ dist/pip-webui-dashboard.d.ts
@@ -1,18 +1,5 @@
 declare module pip.dashboard {
 
-export interface IDashboardTile {
-    options: any;
-    color: string;
-    size: Object | string | number;
-}
-export class DashboardTile implements IDashboardTile {
-    options: any;
-    color: string;
-    size: Object | string | number;
-    constructor();
-}
-
-
 export class AddTileDialog {
     title: string;
     icon: string;
@@ -40,6 +27,18 @@ export interface IAddTileDialogprovider {
 }
 
 
+export interface IDashboardTile {
+    options: any;
+    color: string;
+    size: Object | string | number;
+}
+export class DashboardTile implements IDashboardTile {
+    options: any;
+    color: string;
+    size: Object | string | number;
+    constructor();
+}
+
 
 export class TileConfigDialogController {
     params: any;
@@ -62,6 +61,7 @@ export interface ITileConfigDialogOptions extends angular.material.IDialogOption
     extensionUrl?: string;
     event?: any;
 }
+
 
 
 export const DEFAULT_TILE_WIDTH: number;
@@ -114,13 +114,15 @@ export class DragTileService implements IDragTileService {
 
 
 
-
 export class MenuTileService extends DashboardTile {
     menu: any;
     constructor();
     callAction(actionName: any, params: any, item: any): void;
     changeSize(params: any): void;
 }
+
+
+
 
 
 
@@ -191,12 +193,10 @@ export class TilesGridService implements ITilesGridService {
     updateTileOptions(opts: any): any;
 }
 
-
 export interface ITileTemplateService {
     getTemplate(source: any, tpl?: any, tileScope?: any, strictCompile?: any): any;
     setImageMarginCSS($element: any, image: any): void;
 }
-
 
 }
 
