5aef834ae02f851190f9181a06f6c7f35eadf9ef
Anastas Fonotov <anastasf@gmail.com>
2017-07-19 18:28:42 +0300

    'rebuild'

--- dist/pip-webui-dashboard.d.ts
+++ dist/pip-webui-dashboard.d.ts
@@ -63,7 +63,6 @@ export interface ITileConfigDialogOptions extends angular.material.IDialogOption
 }
 
 
-
 export const DEFAULT_TILE_WIDTH: number;
 export const DEFAULT_TILE_HEIGHT: number;
 export const UPDATE_GROUPS_EVENT = "pipUpdateDashboardGroupsConfig";
@@ -109,6 +108,7 @@ export class DragTileService implements IDragTileService {
     getOptions(): any;
     setOptions(options: any): any;
 }
+
 
 
 
